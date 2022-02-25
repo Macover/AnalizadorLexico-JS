8c4f83eec08011cbc7fd0b64a86c70da998804d5
Isai Rodriguez <macover1507@hotmail.com>
2022-02-24 21:38:38 -0600

    table added with events

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-
-
+const table = document.querySelector("#table");
 const button = document.querySelector("#button");
+const input = document.querySelector("#input");
+const tBodyTable = document.querySelector("tbody");
 
-button.addEventListener("click",()=>{
+let inputValueLength = 0;
+let inputString = "";
+
+button.addEventListener("click", () => {
+    table.style.opacity = "1";
+});
+
+input.addEventListener("keyup", (e) => {
+    // table.style.opacity = "0"; it shows up when a key is pressed    
+    inputString = input.value;
+    console.log(inputString)
+    inputValueLength = input.value.length;
+
+    console.log(inputValueLength)
+
+    let HTML_CODE = `
+        <tr>
+            <th>${inputString[inputValueLength - 1]}</th>
+            <th>simboloJS</th>
+        </tr>    
+        `;
+    tBodyTable.innerHTML += HTML_CODE;
 
 })
+
+
+
