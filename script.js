2d3ae4acd43a46c83423b212c10b36086bf1f943
Isai Rodriguez <macover1507@hotmail.com>
2022-03-01 19:48:39 -0600

    final solution founded

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const table = document.querySelector("#table");
 const button = document.querySelector("#button");
-const input = document.querySelector("#input");
+const input = document.getElementById("input");
 const tBodyTable = document.querySelector("tbody");
 const errorMessage = document.querySelector("#errorMessage");
 const icon = document.querySelector("#icon");
@@ -8,32 +8,41 @@ const icon = document.querySelector("#icon");
 let inputValue = "";
 let inputValueLength = 0;
 let inputString = "";
+let result = 0;
 
 const validateParenthesis = (string)=>{
     let i;
-    let va = 0;
-    for (i = 0; i < s.length; i++){
-        if(s.substring(i,i+1) == '('){
-            va++;
+    let accountant = 0;
+    for (i = 0; i < string.length; i++){
+        if(string.substring(i,i+1) == '('){ //if there's a '(' => The variable 'accountant' increases in one.
+            accountant++;
         }
-        else if(s.substring(i,i+1) == ')'){
-            va--;
+        else if(string.substring(i,i+1) == ')'){ //if there's a ')' => The variable 'accountant' dicreases in one.
+            accountant--;
         }        
-        if (va < 0){
+        if (accountant < 0){ // if the user type ')' first
             return false;
         }
     }        
-    return va == 0;
+    return accountant == 0; // The function returns a boolean
 }
 
 button.addEventListener("click", (e) => {
-    // table.style.opacity = "1";
     e.preventDefault();    
+    table.style.opacity = "1";
+    
+    if(button.classList.contains("hover-button")){
+        button.classList.remove("hover-button");
+        button.classList.add("disabled-button");
+    }        
+    if(!button.hasAttribute("disabled")){
+        button.setAttribute("disabled",true);
+    }
+
     inputValue = input.value;
-    inputValueLength = input.value.length;
+    inputValueLength = input.value.length;  
 
     let fragment = document.createDocumentFragment();
-
     
     for (let i = 0; i < inputValueLength; i++) {
         
@@ -72,24 +81,85 @@ button.addEventListener("click", (e) => {
             fragment.appendChild(trElement);
         }        
     }
+    let trElement = document.createElement("tr");
+    let thElement1 = document.createElement("th");
+    let thElement2 = document.createElement("th");
+
+    thElement1.innerHTML = "Resultado"
+    thElement2.innerHTML = result;
+
+    if(result != 0){
+        thElement1.style.backgroundColor = "#0F0";
+        thElement2.style.backgroundColor = "#0F0";    
+    }else{
+        thElement1.style.backgroundColor = "#F00";
+        thElement2.style.backgroundColor = "#F00";
+    }
+    
+
+    trElement.appendChild(thElement1);      
+    trElement.appendChild(thElement2);      
+    fragment.appendChild(trElement);
     tBodyTable.appendChild(fragment);
 });
 
-input.addEventListener("keyup", (e) => {
+let isValidated = false;
 
+input.addEventListener('input', function () {
+    var error = true;
+    try{        
+        if (/^[\d-+/*()]+$/.test(input.value)) {                        
+            result = eval(input.value);
+            error = false;
+            if (input.classList.contains("bottom-wrong-input")) {
+                input.classList.remove("bottom-wrong-input");
+                input.classList.add("bottom-correct-input");
+            } else {
+                input.classList.add("bottom-correct-input");
+            }
+            icon.src = "images/correct.png";
+            if(button.classList.contains("disabled-button")){
+                button.classList.remove("disabled-button");
+                button.classList.add("hover-button");
+            }
+            if(button.hasAttribute("disabled")){                
+                button.removeAttribute("disabled",false);
+            }
+        }
+    } catch (err) { }
+    if (error){
+        result = 0;                
+        if (input.classList.contains("bottom-correct-input")) {
+            input.classList.remove("bottom-correct-input");
+            input.classList.add("bottom-wrong-input");
+        } else {
+            input.classList.add("bottom-wrong-input");
+        }
+        icon.src = "images/remove.png";
+        if(button.classList.contains("hover-button")){
+            button.classList.remove("hover-button");
+            button.classList.add("disabled-button");
+        }        
+        if(!button.hasAttribute("disabled")){
+            button.setAttribute("disabled",true);
+        }
+    } 
+});
+
+input.addEventListener("keyup", (e) => {
+    table.style.opacity = "0";    
     // Validation for characters
     if (isCharacter(e.key)) {
-        input.value = " "
+        input.value = "";
         errorMessage.textContent = "Error: El input no acepta letras.";
         errorMessage.style.opacity = "1";
     } else {
         errorMessage.style.opacity = "0";
     }
-
-
-    // evaluateString(input.value);
-
+    tBodyTable.innerHTML = "";
 });
+
+
 
 // VALIDATION FUNCTIONS
 
@@ -113,7 +183,7 @@ const isParenthesis = (string) =>{
 }
 
 const isCharacter = (string) => {
-    if (string == "Shift" || string == "Backspace") { return false; }
+    if (string == "Shift" || string == "Backspace" || string == "ArrowLeft" || string == "ArrowRight" || string == "Home" || string == "End") { return false; }
     else { let ascii = string.toUpperCase().charCodeAt(0); return ascii > 64 && ascii < 91; }
 }
 const isANumber = (string) => {
@@ -127,95 +197,95 @@ const isOperator = (string) => {
 
 //Test functions
 
-let rules = {
-    "S": [
-        ["The", "N", "V"]
-    ],
-    "N": [
-        ["cat"],
-        ["dog"]
-    ],
-    "V": [
-        ["meows"],
-        ["barks"]
-    ]
-};
-const evaluateCFG = (start, expansion) => {
-    if (rules[start]) {
-        let rnd = Math.floor(Math.random() * rules[start].length);
-        console.log("random", rnd)
-        let pick = rules[start][rnd];
-        console.log(pick);
-        for (let i = 0; i < pick.length; i++) {
-            evaluateCFG(pick[i], expansion);
-        }
-    } else {
-        expansion.push(start);
-    }
-    return expansion.join(" ");
-}
+// let rules = {
+//     "S": [
+//         ["The", "N", "V"]
+//     ],
+//     "N": [
+//         ["cat"],
+//         ["dog"]
+//     ],
+//     "V": [
+//         ["meows"],
+//         ["barks"]
+//     ]
+// };
+// const evaluateCFG = (start, expansion) => {
+//     if (rules[start]) {
+//         let rnd = Math.floor(Math.random() * rules[start].length);
+//         console.log("random", rnd)
+//         let pick = rules[start][rnd];
+//         console.log(pick);
+//         for (let i = 0; i < pick.length; i++) {
+//             evaluateCFG(pick[i], expansion);
+//         }
+//     } else {
+//         expansion.push(start);
+//     }
+//     return expansion.join(" ");
+// }
 
-// console.table(rules)
+// // console.table(rules)
 
-// let start = "S";
-// let expansion = [];
-// let result = evaluateCFG(start, expansion);
-// console.log(result)
+// // let start = "S";
+// // let expansion = [];
+// // let result = evaluateCFG(start, expansion);
+// // console.log(result)
 
 
-const evaluateString = (INPUT_STRING) => {
+// const evaluateString = (INPUT_STRING) => {
 
-    let string = INPUT_STRING;
-    let initialState = 1;
-    let finalState = 1;
-    let currentState = initialState;
+//     let string = INPUT_STRING;
+//     let initialState = 1;
+//     let finalState = 1;
+//     let currentState = initialState;
 
-    let bool = true;
-    let accountant = 0;
+//     let bool = true;
+//     let accountant = 0;
 
-    while (bool) {
-        if (accountant > string.length - 1) {
-            bool = false;
-            break;
-        }
-        if (currentState == 1) {
-            if (string[accountant] == 1) {
-                currentState = 1;
-            }
-            if (string[accountant] == 0) {
-                currentState = 2;
-            }
-            accountant++;
-            continue;
-        }
-        if (currentState == 2) {
-            if (string[accountant] == 1) {
-                currentState = 2;
-            }
-            if (string[accountant] == 0) {
-                currentState = 1;
-            }
-            accountant++;
-            continue;
-        }
-    }
-    if (currentState == finalState) {
-        if (input.classList.contains("bottom-wrong-input")) {
-            input.classList.remove("bottom-wrong-input");
-            input.classList.add("bottom-correct-input");
-        } else {
-            input.classList.add("bottom-correct-input");
-        }
-        icon.src = "images/correct.png";
-    } else {
-        if (input.classList.contains("bottom-correct-input")) {
-            input.classList.remove("bottom-correct-input");
-            input.classList.add("bottom-wrong-input");
-        } else {
-            input.classList.add("bottom-wrong-input");
-        }
-        icon.src = "images/remove.png";
-    }
-}
+//     while (bool) {
+//         if (accountant > string.length - 1) {
+//             bool = false;
+//             break;
+//         }
+//         if (currentState == 1) {
+//             if (string[accountant] == 1) {
+//                 currentState = 1;
+//             }
+//             if (string[accountant] == 0) {
+//                 currentState = 2;
+//             }
+//             accountant++;
+//             continue;
+//         }
+//         if (currentState == 2) {
+//             if (string[accountant] == 1) {
+//                 currentState = 2;
+//             }
+//             if (string[accountant] == 0) {
+//                 currentState = 1;
+//             }
+//             accountant++;
+//             continue;
+//         }
+//     }
+//     if (currentState == finalState) {
+//         if (input.classList.contains("bottom-wrong-input")) {
+//             input.classList.remove("bottom-wrong-input");
+//             input.classList.add("bottom-correct-input");
+//         } else {
+//             input.classList.add("bottom-correct-input");
+//         }
+//         icon.src = "images/correct.png";
+//     } else {
+//         if (input.classList.contains("bottom-correct-input")) {
+//             input.classList.remove("bottom-correct-input");
+//             input.classList.add("bottom-wrong-input");
+//         } else {
+//             input.classList.add("bottom-wrong-input");
+//         }
+//         icon.src = "images/remove.png";
+//     }
+// }
 
 
